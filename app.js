3a22d176d889ed06b3bc904962e4f133ffdf3f46
mnb14 <mirza.s1415@gmail.com>
2023-02-04 12:44:47 +0100

    Added functionality. Able to write down a task and add it to a list. Task creates a remove button that we can use to remove it from the list.

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 let addTaskBtn = document.getElementById("task-btn");
 let taskInput = document.getElementById("input-task");
 let taskList = document.getElementById("task-list");
+
+addTaskBtn.addEventListener("click", function () {
+  let task = taskInput.value;
+  let taskItem = document.createElement("li");
+  taskItem.textContent = task;
+  const removeBtn = document.createElement("button");
+  removeBtn.textContent = "Remove";
+  removeBtn.addEventListener("click", function () {
+    taskList.removeChild(taskItem);
+  });
+
+  taskItem.appendChild(removeBtn);
+  taskList.append(taskItem);
+  taskInput.value = "";
+});
